3abd2aa3772542a1813b11c2baf2c8a2cbf486e7
noodles <eating8noodles@gmail.com>
2025-05-06 18:36:29 +0200

    250410: image-analysis-lab2-1_7

---
+++ SSY098-Image Ananlysis/lab_3/functions.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+def affine_test_case(N: int):
+    np.random.seed(42)
+
+    pts = np.column_stack((
+        # np.random.randint(0, 11, size=N),
+        np.random.uniform(0, 11, size=N),
+        np.random.uniform(0, 11, size=N)
+    )).T
+
+    A_true = np.column_stack((
+        np.random.uniform(-10, 11, size=2),
+        np.random.uniform(-10, 11, size=2)
+    ))
+
+    t_true = np.random.uniform(-10, 11, size=(2, 1))
+
+    pts_tilde = A_true @ pts + t_true
+
+    # print(f'{pts},\n {pts_tilde},\n {A_true},\n {t_true}')
+    return pts, pts_tilde, A_true, t_true
+
+
+def estimate_affine(pts, pts_tilde):
+    A_list = []
+    b_list = []
+
+    for i in range(3):
+        x, y = pts[:, i]
+        x_p, y_p = pts_tilde[:, i]
+
+        A_list.append([x, y, 0, 0, 1, 0])
+        A_list.append([0, 0, x, y, 0, 1])
+        b_list.append(x_p)
+        b_list.append(y_p)
+
+    right_A = np.array(A_list)
+    left_b = np.array(b_list)
+
+    h, _, _, _ = np.linalg.lstsq(right_A, left_b, rcond=None)
+
+    A = h[:4].reshape(2, 2)
+    t = h[4:].reshape(2, 1)
+
+    return A, t
+
+def residual_lgths(A, t, pts, pts_tilde):
+
+    pts_predicted = A @ pts + t
+
+    residuals = pts_tilde - pts_predicted
+
+    # lgths = np.sqrt(np.sum(np.pow(residuals, 2), axis=0))
+    lgths = np.sqrt(np.sum(residuals ** 2, axis=0))
+
+    return lgths
+
+def affine_test_case_outlier(outlier_rate, n_samples, image_height, image_width):
+    np.random.seed(42)
+
+    pts = np.column_stack((
+        np.random.uniform(0, image_width, size=n_samples),
+        np.random.uniform(0, image_height, size=n_samples)
+    )).T
+
+    A_true = np.column_stack((
+        np.random.uniform(-1, 1, size=2),
+        np.random.uniform(-1, 1, size=2)
+    ))
+
+    t_true = np.random.uniform(-1, 1, size=(2, 1))
+
+    pts_tilde = A_true @ pts + t_true
+
+    outlier_num = int(n_samples * outlier_rate)
+    indices_to_true = np.random.choice(np.arange(0, n_samples), size=outlier_num, replace=False)
+
+    outlier_idxs = np.zeros(n_samples, dtype=bool)
+    outlier_idxs[indices_to_true] = True
+
+    for i in indices_to_true:
+        pts[0, i] += 10
+        pts[1, i] += 10
+
+    # print(f'{pts},\n {pts_tilde},\n {A_true},\n {t_true}')
+
+    return pts, pts_tilde, A_true, t_true, outlier_idxs
+
+def ransac_fit_affine(pts: np.ndarray, pts_tilde: np.ndarray, thresh: float, n_iter: int = 10000, max_inliers: int = 0):
+
+    A = None
+    t = None
+
+    p = 0.9
+    n = pts.shape[1]
+    k = 0
+
+    inliers_ratio = max(max_inliers / n, 0.5)
+    max_k = np.log(1 - p) / np.log(1 - inliers_ratio ** 3)
+
+    while k < min(max_k, n_iter):
+        permutation = np.random.permutation(n)
+
+        shuffled_pts = pts[:, permutation]
+        shuffled_pts_tilde = pts_tilde[:, permutation]
+
+        A_est, t_est = estimate_affine(shuffled_pts, shuffled_pts_tilde)
+
+        lgths = residual_lgths(A_est, t_est, pts, pts_tilde)
+        inliers_count = np.sum(lgths < thresh)
+
+        if inliers_count > max_inliers:
+            max_inliers = inliers_count
+            A = A_est
+            t = t_est
+            inliers_ratio = max(max_inliers / n, 1e-6)
+            max_k = np.log(1-p) / np.log(1 - inliers_ratio ** 3)
+        k += 1
+
+    return A, t
